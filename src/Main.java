63952e839755a7b6a49312d69997469553d8d03a
Alisa Alexandrova <alisaalexandrova2001@gmail.com>
2022-09-22 16:11:24 +0300

    Сделала ДЗ 1.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    // ДЗ 1.
+    public static void leapYear(int year) {
+        if (year % 4 == 0) {
+            System.out.println(year + " - Год високосный");
+        } else {
+            System.out.println(year + " - Год не високосный");
+        }
     }
+
+    public static void main(String[] args) {
+        int year = 2022;
+        leapYear(year);
+
+
+    }
+
 }
